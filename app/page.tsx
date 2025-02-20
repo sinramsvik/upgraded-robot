76a9687bc8b6c87742ef6e52976cc4bd5b7ae1cc
Sindre Ramsvik <sinramsvik@gmail.com>
2025-02-20 13:32:45 +0100

    cleanup landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
 import Link from "next/link";
 
+const links = [
+  {
+    href: "/giphy",
+    title: "GIF fetcher",
+  },
+  {
+    href: "https://codesandbox.io/p/devbox/aged-firefly-8wg83f?workspaceId=ws_KreS6gpqCFK3D6t8tFS7hS",
+    title: "GIF fetcher (Sandbox)",
+    target: "_blank",
+  },
+  {
+    href: "/profile-form",
+    title: "Profile form",
+  },
+  {
+    href: "/posts",
+    title: "Post list",
+  },
+];
+
 const LinkItem = ({
   href,
   children,
@@ -15,26 +35,24 @@ const LinkItem = ({
       target={target}
       className='text-blue-500 hover:text-blue-700 hover:underline'
     >
-      {children}
+      ➡️ {children}
     </Link>
   );
 };
 
 export default function Home() {
   return (
-    <main className='flex flex-col min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <h1 className='text-4xl font-bold mb-2'>Unfold sandbox 🏝️</h1>
-      <div className='flex gap-4'>
-        <LinkItem href='/giphy'>GIF fetcher</LinkItem>
-        <LinkItem
-          href='https://codesandbox.io/p/devbox/aged-firefly-8wg83f?workspaceId=ws_KreS6gpqCFK3D6t8tFS7hS'
-          target='_blank'
-        >
-          GIF fetcher (Sandbox)
-        </LinkItem>
-        <LinkItem href='/profile-form'>Profile form</LinkItem>
-        <LinkItem href='/posts'>Post list</LinkItem>
-      </div>
+    <main className='flex flex-col max-w-[500px] mx-auto min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <h1 className='text-4xl font-bold mb-4'>Unfold sandbox 🏝️</h1>
+      <ul className='flex flex-col gap-2'>
+        {links.map((link) => (
+          <li key={link.href}>
+            <LinkItem href={link.href} target={link.target}>
+              {link.title}
+            </LinkItem>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
